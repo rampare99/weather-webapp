768450c128d9f597f83c3a279799c1e878c9dfc4
Cosme Fulanito <rampare99@gmail.com>
2020-10-01 23:54:38 -0300

    Added heroku support

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const forecast = require('./utils/forecast')
 
 const app = express()
 
+const port = process.env.PORT || 3000
+
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -87,6 +89,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('App running in port 3000')
+app.listen(port, () => {
+    console.log('App running in port ' + port)
 })
